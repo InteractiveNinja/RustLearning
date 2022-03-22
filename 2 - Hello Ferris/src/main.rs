04131228896167f5ddb5a39d327a19a9d1e7e56a
InteractiveNinja <gabrielnadolny2@gmail.com>
2022-03-22 09:34:10 +0100

    adds ferris say

---
+++ 2 - Hello Ferris/src/main.rs
@@ -0,0 +1,10 @@
+use std::io::{BufWriter, stdout};
+use ferris_says::say;
+fn main() {
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(message.as_bytes(), width, &mut writer).unwrap();
+}
