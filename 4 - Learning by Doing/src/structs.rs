cdc1b198c5fe62b324c8e9e53f06012365560908
InteractiveNinja <gabrielnadolny2@gmail.com>
2022-04-27 11:07:23 +0200

    rectangle stuff with structs

--- 4 - Learning by Doing/src/structs.rs
+++ 4 - Learning by Doing/src/structs.rs
@@ -2,11 +2,31 @@ use std::fmt;
 use std::fmt::{Formatter, write};
 
 struct Matrix(f32, f32, f32, f32);
+
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+
+struct Rectangle {
+    height: i32,
+    width: i32
+}
 /// Kann ein Matrix Struct umdrehen
 fn reverse(c: Matrix) -> Matrix {
     // deconstruct, kinda sad bei einem struct lol
     let Matrix { 0: c1, 1: c2, 2: c3, 3: c4 } = c;
     Matrix(c4,c3,c2,c1)
+}
+
+fn rect_area(rec: Rectangle) -> i32 {
+    let Rectangle {height,width } = rec;
+    height * width
+}
+
+fn square(point: Point,size: i32) -> Rectangle {
+    Rectangle{height: point.x as i32 * size ,width: point.y as i32 * size}
 }
 
 
@@ -16,9 +36,15 @@ impl fmt::Display for Matrix {
     }
 }
 
+
 pub fn run() {
     let m = Matrix(9.4,2.1,2.4,3.2);
+    let r = Rectangle{height: 10, width: 5};
 
     println!("Normal {}",m);
     println!("Reverse {}",reverse(m));
+
+    println!("Rect Height {} and Width {}",r.height,r.width);
+    println!("Rect Area {}",rect_area(r))
+
 }
